3faf0938f82d8ed4d955dfd5c6cbb62d5ee01382
zhaopeng08 <zhaopeng08@baidu.com>
2016-12-08 15:24:35 +0800

    Initial Commit

---
+++ TestIOSGitOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestIOSGitOne
+//
+//  Created by Zhao,Peng(BNOPRD) on 2016/12/8.
+//  Copyright © 2016年 Zhao,Peng(BNOPRD). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestIOSGitOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestIOSGitOne
+//
+//  Created by Zhao,Peng(BNOPRD) on 2016/12/8.
+//  Copyright © 2016年 Zhao,Peng(BNOPRD). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
